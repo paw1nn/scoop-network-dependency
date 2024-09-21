417599871133545a0ed4fb18f4bd38b522d54bda
xiter22222 <pawanpediredla@gmail.com>
2024-09-21 21:08:37 +0530

    Add internet connectivity check and Scoop package installation

---
+++ .history/csharp/InternetCheck_20240921210615.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Checking internet connectivity...");
+        
+        if (CheckInternetConnection())
+        {
+            Console.WriteLine("Internet is available. Proceeding with package installation.");
+            InstallScoopPackage("example-package");
+        }
+        else
+        {
+            Console.WriteLine("No internet connection. Please connect to the internet.");
+        }
+    }
+
+    static bool CheckInternetConnection()
+    {
+        try
+        {
+            using (var client = new WebClient())
+            using (client.OpenRead("http://www.google.com"))
+            {
+                return true;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    static void InstallScoopPackage(string packageName)
+    {
+        var scoopCommand = $"scoop install {packageName}";
+        ExecuteCommand(scoopCommand);
+    }
+
+    static void ExecuteCommand(string command)
+    {
+        var processStartInfo = new ProcessStartInfo("powershell.exe", $"-Command {command}")
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        var process = Process.Start(processStartInfo);
+        process.WaitForExit();
+        
+        string result = process.StandardOutput.ReadToEnd();
+        Console.WriteLine(result);
+    }
+}
